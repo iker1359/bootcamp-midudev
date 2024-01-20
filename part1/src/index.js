63b9548d364a9b0fc7ed30ff8d2a8a9d563e89ad
iker1359 <iker1359@gmail.com>
2024-01-20 18:54:49 +0100

    Comienzo episodio 4

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Counter = ({contador}) => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return <h1>{contador}</h1>
+
+}
+
+const AppPart1 = () => { 
+
+  const [contador, setContador] = useState(0)
+
+  const isEven = contador % 2 === 0
+  const isEvenMsg = isEven ? "Es Par" : "Es impar" 
+
+  const handleClickAdd = () => { setContador(contador + 1) }
+  const handleClickLess = () => { setContador(contador - 1) }
+  const handleClickReset = () => { setContador(0) }
+
+  return (
+    
+    <>
+      <Counter contador={contador} />
+      <p>{isEvenMsg}</p>
+      <button onClick={handleClickAdd}> Incrementar contador </button>
+      <button onClick={handleClickLess}> Decrementar contador </button>
+      <button onClick={handleClickReset}> Reset </button>
+    </>
+    
+  )
+
+}
+
+const AppPart2 = () => {
+
+
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render(<AppPart2 />);
